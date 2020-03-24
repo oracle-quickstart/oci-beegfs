e1d69a9bc2b78d7cd98e13669434137adb1d41c6
Pinkesh Valdria <pvaldria@yahoo.com>
2020-03-24 08:05:08 -0700

    update zip file for resource manager

--- orm/orm_generate_zip.tf
+++ orm/orm_generate_zip.tf
@@ -4,7 +4,7 @@ variable "save_to" {
 
 data "archive_file" "generate_zip" {
   type        = "zip"
-  output_path = (var.save_to != "" ? "${var.save_to}/orm.zip" : "${path.module}/dist/orm.zip")
+  output_path = (var.save_to != "" ? "${var.save_to}/beegfs.zip" : "${path.module}/dist/beegfs.zip")
   source_dir = "../"
-  excludes    = ["terraform.tfstate", "terraform.tfvars.template", "terraform.tfvars", "provider.tf", ".terraform", "images" , "orm" , ".git"  ]
+  excludes    = ["terraform.tfstate", "terraform.tfvars.template", "terraform.tfvars", "provider.tf", ".terraform", "images" , "orm" , ".git" , "localonly.ior_mpiio_setup.tf" , "terraform.tfstate.backup" , "scripts/passwordless_ssh.sh" , "scripts/ior_install.sh" , "scripts/restart.sh" ]
 }
